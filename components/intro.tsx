94d5a898319191b70273ea3dd994628e26b06595
yoni almozleno <johnalmozlino@gmail.com>
2024-07-01 10:32:26 +0300

    fix spelling mistake

--- components/intro.tsx
+++ components/intro.tsx
@@ -77,7 +77,7 @@ const Intro = () => {
                 setTimeOfLastClick(Date.now());
             }}
             >
-                Contant me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition"/>
+                Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition"/>
             </Link>
             <a className="group bg-white px-7 py-3 flex items-center
             gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10" href="/John Mor Almozlino CV.pdf" download>
